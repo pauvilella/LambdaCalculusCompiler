404b3c02b66a23bd0c3ab7622fd4d48b9b71977d
pauvilella <pau.vilella.st@gmail.com>
2021-07-09 18:03:48 +0200

    FIX subst

--- LambdaHask.hs
+++ LambdaHask.hs
@@ -33,11 +33,23 @@ boundVars (V x) = []
 boundVars (L x lt) = if checkBoundInLT lt x then x : boundVars lt else boundVars lt
 boundVars (A lt1 lt2) = boundVars lt1 ++ boundVars lt2
 
+-- Funcio que mira si una determinada variable es troba dins una llista de variables o no
+-- La faig servir per mirar si la variable de la lambda abstracció que estic substiuint està a dins del lambda terme que es ficarà a dins. Si sí que hi és, vol dir que es faria captura de variables, ja que la lliure quedaria caputrada per la variable de la lambda abstracció.
+checkIfIsInFreeVars :: Var -> [Var] -> Bool
+checkIfIsInFreeVars _ [] = False 
+checkIfIsInFreeVars x (y:ys)
+    | x == y = True 
+    | otherwise = checkIfIsInFreeVars x ys
+
+
 -- FUNCIO SUBST
 subst :: LT -> Var -> LT -> LT
-subst (V x) y lt2 = if x == y then lt2 else V x
+subst (V x) y lt2 = if x == y then lt2 else V x -- Si és una sola variable, i aquesta és la variable a ser subsituida, la substiuim; si no, deixem la variable que hi havia.
 subst (L x lt1) y lt2
-    | x == y = L x lt1
-    | checkBoundInLT lt2 x = subst (L (x++"*") (subst lt1 x (V (x++"*")))) y lt2 -- Aqui detectem que hi hauria captura de variables, per tant el que fem es tornar a fer la substitucio però amb un canvi de variable de x a x* a lt1
-    | otherwise = L x (subst lt1 y lt2)
-subst (A lt1 lt2) y lt3 = A (subst lt1 y lt3) (subst lt2 y lt3)
+    | x == y = L x lt1 -- Vol dir que la variable que hauria de ser substituida no es lliure, ja que la lliga la labmda abstracció, per tant, no es substiuirà res, es queda el mateix lambda terme.
+    | checkIfIsInFreeVars x (freeVars lt2) = subst (L (x++"*") (subst lt1 x (V (x++"*")))) y lt2 -- Aqui detectem que hi hauria captura de variables, per tant el que fem eés tornar a fer la substitució però amb un canvi de variable de x a x* a lt1.
+    | otherwise = L x (subst lt1 y lt2) -- Deixem la lambda abstracció normal i fem la substitució a la part de dins de l'abstracció
+subst (A lt1 lt2) y lt3 = A (subst lt1 y lt3) (subst lt2 y lt3) -- Simplement apliquem la substituicó als dos lambda termes de l'aplicació.
+
+test_subst_sense_captura = subst (L "x" (V "y")) "y" (L "a" (V "b"))
+test_subst_amb_captura = subst (L "x" (V "y")) "y" (L "y" (V "x"))
